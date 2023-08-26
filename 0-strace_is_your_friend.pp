d27bbbbb1e9cb01e1dae4c58598b4cbb61bf6d8e
Mohamed Ahmed Yasin <116743995+MohamedAYasin@users.noreply.github.com>
2023-08-26 17:29:56 +0200

    Delete 0-strace_is_your_friend.pp

--- 0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# using puppet code to rename a file and create a file
-
-exec { 'fix file name':
-  command  => 'sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php',
-  provider => 'shell'
-}
